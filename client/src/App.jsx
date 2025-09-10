48024b9c7cf4fffc05c7a0c784ee2ed10a15634c
Tushal-Mendpara <tushalmendpara@gmail.com>
2025-09-10 14:47:35 +0530

    Revert "Update project files"
    This reverts commit a4b96a96475ac6d71d3d3fdebcb0758575e8eafc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 
-
-// this is main page of the app
-
 const App = () => {
   return (
-    // return the main page code which includes all main page html code bwelow
-    <div>
-      <h1>Welcome to Shabd Setu</h1>
-      <p>Your bridge to language learning.</p>
-    </div>
+    <div> </div>
   )
 }
 
