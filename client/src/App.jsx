a4b96a96475ac6d71d3d3fdebcb0758575e8eafc
Tushal-Mendpara <tushalmendpara@gmail.com>
2025-09-10 14:45:31 +0530

    Update project files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
+// this is main page of the app
+
 const App = () => {
   return (
-    <div> </div>
+    // return the main page code which includes all main page html code bwelow
+    <div>
+      <h1>Welcome to Shabd Setu</h1>
+      <p>Your bridge to language learning.</p>
+    </div>
   )
 }
 
